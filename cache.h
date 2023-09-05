32714021d785ead6165b05fe01a6320f42dd36c7
V_markov <mvv1993Vasya@gmail.com>
2023-09-06 01:55:26 +0300

    ARC - Adding ghost for mru.

--- cache.h
+++ cache.h
@@ -10,34 +10,42 @@ template <typename T, typename KeyT = int>
 class Cache{
 private:
     size_t m_size;
-
-    std::list<std::pair<KeyT, T>> m_cache;
+    size_t mru_size, mfu_size;
+    std::list<std::pair<KeyT, T>> mru, mfu;
+    std::list<std::pair<KeyT, T>> mru_ghost, mfu_ghost;
     using ListIt = typename std::list<std::pair<KeyT, T>>::iterator;
 
     std::unordered_map<KeyT, ListIt> m_hash;
+    std::unordered_map<KeyT, ListIt> m_hash_g;
 public:
-    Cache(size_t size): m_size(size) {}
+    Cache(size_t size): m_size(size) {mru_size = m_size; mfu_size = m_size;}
 
-    bool full() const {return (m_cache.size() == m_size);}
+    bool full(const std::unordered_map<KeyT, ListIt>& list, size_t size) const {return (list.size() == size);}
 
     template<typename F_ptr>
     bool lookup_update(int key, F_ptr slow_get_page)
     {
         auto hit = m_hash.find(key);
-        if(hit == m_hash.end()) {
-            if (full()) {
-                m_hash.erase(m_cache.back().first);
-                m_cache.pop_back();
+        if(hit == std::end(m_hash)) {
+            if(full(mru, mru_size)) {
+                m_hash_g.emplace(mru.back().first, std::end(mru));
+                m_hash.erase(mru.back().first);
+                mru_ghost.emplace_front(mru.back());
+                mru.pop_back();
             }
+            else if(full(mru_ghost, mru_size)) {
+                m_hash_g.erase(mru_ghost.back().first);
+                mru_ghost.pop_back();
+            }
+            mru.emplace_front(key, slow_get_page(key));
+            m_hash.emplace(key, std::begin(mru));
 
-            m_cache.emplace_front(key, slow_get_page(key));
-            m_hash.emplace(key, std::begin(m_cache));
             return false;
         }
         else {
             auto eltit = hit->second;
-            if(eltit != std::begin(m_cache))
-                m_cache.splice(std::begin(m_cache), m_cache, eltit, std::next(eltit));
+            if(eltit != std::begin(mru))
+                mru.splice(std::begin(mru), mru, eltit, std::next(eltit));
             return true;
         }
     }
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
 {
     size_t hits{0};
     size_t m = 4, n = 0;
-
+    std::unordered_map<int, std::pair<int, int>> m_hash;
     my::Cache<int> c{m};
     std::vector<int> indices = {1, 2, 3, 4, 1, 2, 5, 1, 2, 4, 3, 4};
 //    for(auto i = 0; i < n; ++i) {
