98463fb01f9d6331fe76ac82b3ecb0d52a298d71
V_markov <mvv1993Vasya@gmail.com>
2023-09-09 18:26:27 +0300

    Adding binary

--- main.cpp
+++ main.cpp
@@ -3,42 +3,39 @@
 #include "arc.h"
 #include "ideal_cache.h"
 
+//#define DEFINE_ARC
+
 int main()
 {
-    size_t m = 5;
-    caches::ARC<int> arc{m};
-    caches::IdealCache<int> ideal;
-    std::vector<int> pages;
 
+    size_t m, n;
+    std::cin >> m >> n;
+    std::vector<int> pages;
+    for(size_t i = 0; i < n; ++i) {
+        int tmp;
+        std::cin >> tmp;
+        pages.push_back(tmp);
+    }
+#ifdef DEFINE_ARC
+
+
+    caches::ARC<int> arc{m};
     auto test {[&](std::vector<int>& pages){
         for(const auto& i : pages) {
             arc.lookup_update(i);
         }
-        arc.print();
+        std::cout << arc.getHits() << std::endl;
         arc.clear();
     }};
-
-    pages = {1, 2, 3, 4, 5, 6};
     test(pages);
 
-    pages = {1, 2, 3, 4, 5, 1};
-    test(pages);
-
-    arc.setSize(4);
-    pages = {1, 2, 3, 4, 5, 1};
-    test(pages);
-
-    arc.setSize(7);
-    pages = {1, 2, 3, 4, 1, 1, 2};
-    test(pages);
-
-    arc.setSize(8);
-    pages = {1, 1, 1, 1, 2, 3, 4, 5, 6, 6, 6, 6, 6, 1, 1};
-    test(pages);
+#else
+    caches::IdealCache<int> ideal;
     for(const auto& i : pages) {
         ideal.lookup_update(i);
     }
-    std::cout << "Ideal cache hits: " << ideal.getHits() << std::endl;
+    std::cout << ideal.getHits() << std::endl;
+#endif
 
     return 0;
 }
