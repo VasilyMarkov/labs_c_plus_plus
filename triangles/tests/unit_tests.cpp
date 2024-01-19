4c117a67f52be989ae3205631329133e2fb3db94
Vasily Markov <mvv1993Vasya@gmail.com>
2024-01-19 16:50:47 +0300

    separable plane test

--- triangles/tests/unit_tests.cpp
+++ triangles/tests/unit_tests.cpp
@@ -3,6 +3,8 @@
 #include <optional>
 #include "linal.hpp"
 #include <iostream>
+#include <fstream>
+
 using namespace testing;
 
 template<typename T>
@@ -60,22 +62,27 @@ TEST(UnitTest, isNotPlane) {
 }
 
 TEST(UnitTest, triangleProjection) {
-    std::vector<int> points = {0,0,0, 1,0,0, 0,1,0,  //z = 0
-                               0,0,0, 1,0,0, 0,0,1,  //y = 0
-                               0,0,0, 0,1,0, 0,0,1,  //x = 0
-                               1,0,0, 0,1,0, 1,0,1,  //x+y = 1
-                               1,0,0, 1,1,0, 0,0,1,  //x+z = 1
-                               1,1,0, 0,1,0, 0,0,1,  //y+z = 1
-                               1,0,0, 0,1,0, 0,0,1}; //x+y+z = 1
-    auto triangles = createTriangles(points);
- 
-    std::vector<Triangle2d> proj = {{Point2d(0,0),Point2d(1,0), Point2d(0,1)},
-                                    {Point2d(0,0),Point2d(1,0), Point2d(0,1)},
-                                    {Point2d(0,0),Point2d(1,0), Point2d(0,1)},
-                                    {Point2d(1,0),Point2d(0,0), Point2d(1,1)},
-                                    {Point2d(1,0),Point2d(1,1), Point2d(0,0)},
-                                    {Point2d(1,1),Point2d(0,1), Point2d(0,0)},
-                                    {Point2d(1,0),Point2d(0,1), Point2d(0,0)}};
+    std::vector<int> points = {
+        0,0,0, 1,0,0, 0,1,0,  //z = 0
+        0,0,0, 1,0,0, 0,0,1,  //y = 0
+        0,0,0, 0,1,0, 0,0,1,  //x = 0
+        1,0,0, 0,1,0, 1,0,1,  //x+y = 1
+        1,0,0, 1,1,0, 0,0,1,  //x+z = 1
+        1,1,0, 0,1,0, 0,0,1,  //y+z = 1
+        1,0,0, 0,1,0, 0,0,1   //x+y+z = 1
+    }; 
+
+    const auto triangles = createTriangles(points);
+
+    std::vector<Triangle2d> proj = {
+        {Point2d(0,0),Point2d(1,0), Point2d(0,1)},
+        {Point2d(0,0),Point2d(1,0), Point2d(0,1)},
+        {Point2d(0,0),Point2d(1,0), Point2d(0,1)},
+        {Point2d(1,0),Point2d(0,0), Point2d(1,1)},
+        {Point2d(1,0),Point2d(1,1), Point2d(0,0)},
+        {Point2d(1,1),Point2d(0,1), Point2d(0,0)},
+        {Point2d(1,0),Point2d(0,1), Point2d(0,0)}
+    };
 
     auto result = true;
     std::vector<int> indexes;
@@ -90,6 +97,53 @@ TEST(UnitTest, triangleProjection) {
         std::cout << "--- Incorrect indexes: " << indexes << "---" << std::endl;
     EXPECT_TRUE(result);
 }
+
+TEST(UnitTest, separablePlane) {
+    double eps = 1e-7;
+    std::vector<double> points = {
+        3,0,0, 0,3,0, 0,0,3,              //base triangle
+        3,0,0, 0,3,6, 0,0,6,              //intersection between 2 points
+        3,0,0, 0,3,0, 3,3,3,              //intersection between 2 lines
+        1.5,1.5,0, 3,3,0, 3,3,3,          //intersection between line and point
+        1,1,1, 3,3,0, 3,3,3,              //intersection between plane and point
+        1.5,1.5,0, 0,1.5,1.5, 3,3,3,      //intersection between plane and line
+        3+eps,0,0, 0,3+eps,0, 0,0,3+eps   //intersection between closed triangles
+    };
+    const auto triangles = createTriangles(points);
+    auto result = true;
+    std::vector<int> indexes;
+
+    for(auto i = 1; i < triangles.size(); ++i) {
+        if(triangles.at(0).separable_plane_from(triangles.at(i))) {
+            result = false;
+            indexes.push_back(i);
+        }
+    }
+    if(!result)
+        std::cout << "--- Incorrect indexes: " << indexes << "---" << std::endl;
+    EXPECT_TRUE(result);
+}
+
+// TEST(UnitTest, separableLine) {
+//     double eps = 1e-7;
+//     std::vector<double> points = {0,0,0, 0,0,0, 0,0,0,
+//                                   1,0,0, 0,0,0, 0,0,0,
+//                                   1,0,0, 2,0,0, 3,0,0,
+//                                   1+eps,0,0, 2+eps,0,0, 3+eps,0,0};
+//     const auto triangles = createTriangles(points);
+//     auto result = false;
+//     std::vector<int> indexes;
+
+//     for(auto i = 0; i < triangles.size(); ++i) {
+//         if(triangles.at(i).isPlane()) {
+//             result = true;
+//             indexes.push_back(i);
+//         }
+//     }
+//     if(result)
+//         std::cout << "--- Incorrect indexes: " << indexes << "---" << std::endl;
+//     EXPECT_FALSE(result);
+// }
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
